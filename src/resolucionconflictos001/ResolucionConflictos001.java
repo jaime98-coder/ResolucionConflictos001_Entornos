0d0a74d06fd452bb2fda2408be9b697f40332402
jaime98-coder <jaime.martin@iesnervion.es>
2026-01-12 12:32:55 +0100

    subida

--- src/resolucionconflictos001/ResolucionConflictos001.java
+++ src/resolucionconflictos001/ResolucionConflictos001.java
@@ -12,6 +12,7 @@ public class ResolucionConflictos001 {
 		apellidos = sc.nextLine();
 		System.out.println("Hola, soy "+nombre+" "+apellidos);
 
+		System.out.println("Hola, soy Gonzalo López Romero");
 		
 	}
 
