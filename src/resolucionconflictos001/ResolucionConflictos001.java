2a6966e78b0e139f9f6a7b85d3c2891ff5b28488
jaime98-coder <jaime.martin@iesnervion.es>
2026-01-12 12:18:16 +0100

    Resolucion Conflictos 2º paso

--- src/resolucionconflictos001/ResolucionConflictos001.java
+++ src/resolucionconflictos001/ResolucionConflictos001.java
@@ -1,9 +1,16 @@
 package resolucionconflictos001;
 
+import java.util.Scanner;
+
 public class ResolucionConflictos001 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		Scanner sc = new Scanner (System.in);
+		String nombre;
+		String apellidos;
+		nombre = sc.nextLine();
+		apellidos = sc.nextLine();
+		System.out.println("Hola, soy "+nombre+" "+apellidos);
 
 	}
 
