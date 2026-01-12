7ca08036f7cfd195e2f49dd5ee872d302ec2620a
gonzalolopez77 <gonzalo.lopez@iesnervion.es>
2026-01-12 12:17:17 +0100

    .

--- src/resolucionconflictos001/ResolucionConflictos001.java
+++ src/resolucionconflictos001/ResolucionConflictos001.java
@@ -5,6 +5,8 @@ public class ResolucionConflictos001 {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
+		System.out.println("Hola, soy Gonzalo López Romero");
+		
 	}
 
 }
